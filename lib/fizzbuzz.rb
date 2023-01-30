adf5aeecadb2d75de8311008b63f32214871f905
Chang <huynhchang.one@gmail.com>
2023-01-30 16:06:42 +0000

    updated commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,20 +1,24 @@
-
-#fizzbuzz method is taking a parameter
-
-def fizzbuzz(n)
-  (0..n).each do |num|
-    if num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    elsif num % 3 == 0
-      puts "Fizz"
-    else 
-      puts num
-    end
-  end
+def fizzbuzz(number)
+  "fizz"
 end
 
-puts "Type in your number"
-number = gets.chomp
-fizzbuzz(number.to_i)
+fizzbuzz
+
+#fizzbuzz method is taking a parameter
+# def fizzbuzz(n)
+#   (0..n).each do |num|
+#     if num % 3 == 0 && num % 5 == 0
+#       puts "FizzBuzz"
+#     elsif num % 5 == 0
+#       puts "Buzz"
+#     elsif num % 3 == 0
+#       puts "Fizz"
+#     else 
+#       puts num
+#     end
+#   end
+# end
+
+# puts "Type in your number"
+# number = gets.chomp
+# fizzbuzz(number.to_i)
