2499f0f138d44a24cbd901c4b4e8b165b56f7e5d
Chang <huynhchang.one@gmail.com>
2023-01-30 16:45:29 +0000

    updated test files

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,23 +1,11 @@
-#def fizzbuzz(number)
-#  "fizz"
-#end
-
-
-
- def fizzbuzz(n)
-   (0..n).each do |num|
-     if num % 3 == 0 && num % 5 == 0
-       puts "FizzBuzz"
-     elsif num % 5 == 0
-       puts "buzz"
-     elsif num % 3 == 0
-       puts "fizz"
-     #else 
-      # puts num
-     end
-   end
- end
-
-# puts "Type in your number"
-# number = gets.chomp
-# fizzbuzz(number.to_i)
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    "fizzbuzz"
+  elsif num % 5 == 0
+    "buzz"
+  elsif num % 3 == 0
+    "fizz"
+  else 
+    num
+  end
+end
