920537e4b5444e20941412a7e06fbf98061e71e7
FrancescoGuglielmi <francesco.guglielmi.work@outlook.com>
2023-01-30 16:28:02 +0000

    modified files

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,23 +1,22 @@
-def fizzbuzz(number)
-  "fizz"
-end
+#def fizzbuzz(number)
+#  "fizz"
+#end
 
-fizzbuzz
 
-#fizzbuzz method is taking a parameter
-# def fizzbuzz(n)
-#   (0..n).each do |num|
-#     if num % 3 == 0 && num % 5 == 0
-#       puts "FizzBuzz"
-#     elsif num % 5 == 0
-#       puts "Buzz"
-#     elsif num % 3 == 0
-#       puts "Fizz"
-#     else 
-#       puts num
-#     end
-#   end
-# end
+
+ def fizzbuzz(n)
+   (0..n).each do |num|
+     if num % 3 == 0 && num % 5 == 0
+       puts "FizzBuzz"
+     elsif num % 5 == 0
+       puts "buzz"
+     elsif num % 3 == 0
+       puts "fizz"
+     #else 
+      # puts num
+     end
+   end
+ end
 
 # puts "Type in your number"
 # number = gets.chomp
